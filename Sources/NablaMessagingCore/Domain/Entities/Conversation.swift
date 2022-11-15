d31f4f84a812281738f16f67d198e14da8382253
Mathieu <mathieu.perroud@dreem.com>
2022-11-15 15:46:46 +0100

    [iOS SDK] Added `public` visibility to `Conversation` ctor

--- Sources/NablaMessagingCore/Domain/Entities/Conversation.swift
+++ Sources/NablaMessagingCore/Domain/Entities/Conversation.swift
@@ -10,4 +10,26 @@ public struct Conversation {
     public let patientUnreadMessageCount: Int
     public let pictureUrl: URL?
     public let providers: [ProviderInConversation]
+    
+    public init(
+        id: UUID,
+        title: String?,
+        subtitle: String?,
+        inboxPreviewTitle: String,
+        lastMessagePreview: String?,
+        lastModified: Date,
+        patientUnreadMessageCount: Int,
+        pictureUrl: URL?,
+        providers: [ProviderInConversation]
+    ) {
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.inboxPreviewTitle = inboxPreviewTitle
+        self.lastMessagePreview = lastMessagePreview
+        self.lastModified = lastModified
+        self.patientUnreadMessageCount = patientUnreadMessageCount
+        self.pictureUrl = pictureUrl
+        self.providers = providers
+    }
 }
